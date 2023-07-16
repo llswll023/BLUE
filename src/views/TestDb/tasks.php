4f9935793a17217a53c255431f384a1ad4213aba
llswll023 <llswll023@gmail.com>
2023-07-16 22:25:45 +0900

    test

--- src/views/TestDb/tasks.php
+++ src/views/TestDb/tasks.php
@@ -1,6 +1,6 @@
 <?php require_once APPROOT . '/src/views/include/header.php'; ?>
-    <h1 class="p-3 mb-2 bg-info-subtle text-emphasis-inf text-center">Test Database</h1>
-    <h2>This page tests the connection to a database, using a typical "to-do list app" to perform CRUD operations</h2>
+    <h1 class="p-3 mb-2 bg-info-subtle text-emphasis-inf text-center fw-bold">Test Database</h1>
+    <h2 class="text-center">This page tests the connection to a database, using a typical "to-do list app" to perform CRUD operations</h2>
     <hr>
     <h3>[CREATE] Add new task</h3>
     <form action="<?= URLROOT; ?>/test/add-task" method="post">
@@ -21,8 +21,19 @@
                 <?php else : ?>
                     <?= $task->description; ?> &emsp;
                     <form action="<?= URLROOT; ?>/test/<?= $task->id; ?>/mark-done" method="post">
-                        <input type="submit" class="btn btn-success btn-sm" value="Mark as done">
+                        <input type="submit" class="btn btn-secondary btn-sm" value="Mark as done">
                     </form>
+                    <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+                     <div class="toast-header">
+                         <img src="..." class="rounded me-2" alt="...">
+                         <strong class="me-auto">Bootstrap</strong>
+                         <small>11 mins ago</small>
+                         <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                     </div>
+                     <div class="toast-body">
+                         Hello, world! This is a toast message.
+                     </div>
+                    </div>
                 <?php endif; ?>
                 
                 <form action="<?= URLROOT; ?>/test/<?= $task->id; ?>/delete" method="post">
