a2764e78d50d4859f02f6778240b5c53d7ae30ca
llswll023 <llswll023@gmail.com>
2023-07-16 21:32:33 +0900

    footer header

--- src/views/include/footer.php
+++ src/views/include/footer.php
@@ -1,5 +1,5 @@
-<script src="<?= URLROOT; ?>/js/main.js"></script>
-<script src="js/jquery.min.js"></script>
-<script src="js/bootstrap.js"></script>
+    <script src="<?= URLROOT; ?>/js/main.js"></script>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.js"></script>
 </body>
 </html>
