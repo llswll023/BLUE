593c4299fddee32d6542b201bfaf4379f7fb6e70
llswll023 <llswll023@gmail.com>
2023-07-16 21:33:22 +0900

    home

--- src/views/Index/home.php
+++ src/views/Index/home.php
@@ -1,4 +1,4 @@
 <?php require_once APPROOT . '/src/views/include/header.php'; ?>
-    <div class="fs-2 fw-bold p-3 mb-2 bg-primary text-white text-center">Main Page</div>
+    <h1>Main Page</h1>
     <p class="fw-italic">This is the homepage</p>
 <?php require_once APPROOT . '/src/views/include/footer.php'; ?>
