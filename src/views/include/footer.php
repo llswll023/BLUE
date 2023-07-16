0340bb1947afa3b362074d37c95080b8a75b7023
llswll023 <llswll023@gmail.com>
2023-07-16 21:13:27 +0900

    footer header

--- src/views/include/footer.php
+++ src/views/include/footer.php
@@ -1,3 +1,5 @@
 <script src="<?= URLROOT; ?>/js/main.js"></script>
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.js"></script>
 </body>
 </html>
--- src/views/include/header.php
+++ src/views/include/header.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link rel="stylesheet" href="<?= URLROOT;?>/public/css/style.css">
-    <title> <?= SITENAME; ?> </title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<title> <?= SITENAME; ?> </title>
 </head>
 <body>
 	<nav>
