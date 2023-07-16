a3312ad6d378370d87a6f1516d0c1a553a95a11a
llswll023 <llswll023@gmail.com>
2023-07-16 21:07:45 +0900

    testdb

--- src/views/TestDb/tasks.php
+++ src/views/TestDb/tasks.php
@@ -2,10 +2,14 @@
     <h1>Test Database</h1>
     <h2>This page tests the connection to a database, using a typical "to-do list app" to perform CRUD operations</h2>
     <hr>
-    <h3>[CREATE] AdD new task</h3>
-    <form action="<?= URLROOT; ?>/test/add-task" method="post">
-        <input type="text" name="new_task" placeholder="Description...">
-        <input type="submit" value="Add task">
+    <h3>[CREATE] Add new task</h3>
+    <form>
+     <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Add new task</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <div id="emailHelp" class="form-text">Write Description</div>
+     </div>
+     <button type="submit" class="btn btn-primary">Submit</button>
     </form>
     <h3>[READ] Get all tasks:</h3>
 
@@ -17,12 +21,12 @@
                 <?php else : ?>
                     <?= $task->description; ?> &emsp;
                     <form action="<?= URLROOT; ?>/test/<?= $task->id; ?>/mark-done" method="post">
-                        <button type="button" class="btn btn-primary">Mark as done</button>
+                        <input type="submit" class="btn btn-primary" value="Mark as done">
                     </form>
                 <?php endif; ?>
                 
                 <form action="<?= URLROOT; ?>/test/<?= $task->id; ?>/delete" method="post">
-                    <input type="submit" value="Delete">
+                    <input type="submit" class="btn btn-primary" value="Delete">
                 </form>                
             </li>
         <?php endforeach; ?>
