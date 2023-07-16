fc1f72de01347c3b9c25e383bfdef20d0ce5217b
llswll023 <llswll023@gmail.com>
2023-07-16 22:32:39 +0900

    test

--- src/views/TestDb/tasks.php
+++ src/views/TestDb/tasks.php
@@ -1,8 +1,8 @@
 <?php require_once APPROOT . '/src/views/include/header.php'; ?>
     <h1 class="p-3 mb-2 bg-info-subtle text-emphasis-inf text-center fw-bold">Test Database</h1>
-    <h2 class="text-center">This page tests the connection to a database, using a typical "to-do list app" to perform CRUD operations</h2>
+    <h2 class="fst-italic text-center">This page tests the connection to a database, using a typical "to-do list app" to perform CRUD operations</h2>
     <hr>
-    <h3>[CREATE] Add new task</h3>
+    <h3 class="fw-bold">[CREATE] Add new task</h3>
     <form action="<?= URLROOT; ?>/test/add-task" method="post">
      <div class="mb-3">
         <label for="InputTask" class="form-label">Add new task</label>
@@ -11,8 +11,9 @@
      </div>
      <input type="submit" class="btn btn-primary" value="Add task">
     </form>
-    <p><h3>[READ] Get all tasks:</h3></p>
-
+    <br><br>
+    <h3 class="fw-bold">[READ] Get all tasks:</h3>
+    
     <ul>
         <?php foreach($data as $task): ?>
             <li> 
@@ -21,18 +22,8 @@
                 <?php else : ?>
                     <?= $task->description; ?> &emsp;
                     <form action="<?= URLROOT; ?>/test/<?= $task->id; ?>/mark-done" method="post">
-                        <input type="submit" class="btn btn-secondary btn-sm" value="Mark as done">
-                        <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-                            <div class="toast-header">
-                                <img src="..." class="rounded me-2" alt="...">
-                                <strong class="me-auto">Bootstrap</strong>
-                                <small>11 mins ago</small>
-                                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-                            </div>
-                            <div class="toast-body">
-                                Hello, world! This is a toast message.
-                            </div>
-                        </div>
+                        <input type="submit" class="btn btn-info btn-sm" value="Mark as done">
+                        
                     </form>
 
                 <?php endif; ?>
