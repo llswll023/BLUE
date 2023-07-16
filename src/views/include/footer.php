34262867693d63a29b37324519d4a7c6bc94601b
llswll023 <llswll023@gmail.com>
2023-07-16 21:51:27 +0900

    boots

--- src/views/include/footer.php
+++ src/views/include/footer.php
@@ -1,5 +1,5 @@
     <script src="<?= URLROOT; ?>/js/main.js"></script>
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.js"></script>
+    <script src="C:\xampp\htdocs\BLUE\src\views\include\bootstrap\js\jquery.min.js"></script>
+    <script src="C:\xampp\htdocs\BLUE\src\views\include\bootstrap\js\bootstrap.js"></script>
 </body>
 </html>
