0d5244ccde0b5d113b78a37287674851706a5b22
Juhyeong Ko <dury.ko@gmail.com>
2023-07-22 23:32:29 +0900

    Update index css path

--- src/views/include/footer.php
+++ src/views/include/footer.php
@@ -1,5 +1,5 @@
-    <script src="<?= URLROOT; ?>/js/main.js"></script>
-    <script src="/public/bootstrap/js/jquery.min.js"></script>
-    <script src="/public/bootstrap/js/bootstrap.js"></script>
+    <script src="<?= URLROOT;?>/js/main.js"></script>
+    <script src="<?= URLROOT;?>/public/bootstrap/js/jquery.min.js"></script>
+    <script src="<?= URLROOT;?>/public/bootstrap/js/bootstrap.js"></script>
 </body>
 </html>
